21f8ddd6fe362441bd0c382a10c3416484a151a0
eshaalmajid01 <eshaalmajid01@gmail.com>
2024-08-17 10:11:48 -0400

    We added functions for omni drive and mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summer_workshop/SpiderBitsDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summer_workshop/SpiderBitsDriveTrain.java
@@ -1,5 +1,57 @@
 package org.firstinspires.ftc.teamcode.summer_workshop;
 
 //TODO - change this
-public class SpiderBitsDriveTrain {
+public class SpiderBitsDriveTrain implements DriveTrain {
+    @Override
+    public void moveForward(RobotHardware hardware) {
+        hardware.leftFront.setPower(0.5);
+        hardware.rightFront.setPower(0.5);
+        hardware.leftBack.setPower(0.5);
+        hardware.rightBack.setPower(0.5);
+    }
+
+    @Override
+    public void moveBackwards(RobotHardware hardware) {
+        hardware.leftFront.setPower(-0.5);
+        hardware.rightFront.setPower(-0.5);
+        hardware.leftBack.setPower(-0.5);
+        hardware.rightBack.setPower(-0.5);
+    }
+
+    @Override
+    public void strafeRight(RobotHardware hardware) {
+        hardware.leftFront.setPower(0.5);
+        hardware.rightFront.setPower(-0.5);
+        hardware.leftBack.setPower(-0.5);
+        hardware.rightBack.setPower(0.5);
+    }
+
+    @Override
+    public void strafeLeft(RobotHardware hardware) {
+        hardware.leftFront.setPower(-0.5);
+        hardware.rightFront.setPower(0.5);
+        hardware.leftBack.setPower(0.5);
+        hardware.rightBack.setPower(-0.5);
+    }
+    @Override
+    public void turnRight(RobotHardware hardware) {
+        hardware.leftFront.setPower(0.5);
+        hardware.rightFront.setPower(-0.5);
+        hardware.leftBack.setPower(0.5);
+        hardware.rightBack.setPower(-0.5);
+    }
+    @Override
+    public void turnLeft(RobotHardware hardware) {
+        hardware.leftFront.setPower(-0.5);
+        hardware.rightFront.setPower(0.5);
+        hardware.leftBack.setPower(-0.5);
+        hardware.rightBack.setPower(0.5);
+    }
+    @Override
+    public void stop(RobotHardware hardware) {
+        hardware.leftFront.setPower(0);
+        hardware.rightFront.setPower(0);
+        hardware.leftBack.setPower(0);
+        hardware.rightBack.setPower(0);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summer_workshop/SpiderByteDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summer_workshop/SpiderByteDriveTrain.java
@@ -1,5 +1,51 @@
 package org.firstinspires.ftc.teamcode.summer_workshop;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 //TODO - change this
-public class SpiderByteDriveTrain {
+public class SpiderByteDriveTrain implements DriveTrain{
+
+
+
+
+    @Override
+    public void moveForward(RobotHardware hardware) {
+        hardware.leftFront.setPower(0.5);
+        hardware.rightFront.setPower(0.5);
+        hardware.leftBack.setPower(0.5);
+        hardware.rightBack.setPower(0.5);
+    }
+
+    @Override
+    public void moveBackwards(RobotHardware hardware) {
+        hardware.leftFront.setPower(-0.5);
+        hardware.rightFront.setPower(-0.5);
+        hardware.leftBack.setPower(-0.5);
+        hardware.rightBack.setPower(-0.5);
+    }
+
+    @Override
+    public void turnRight(RobotHardware hardware) {
+        hardware.leftFront.setPower(-0.5);
+        hardware.rightFront.setPower(0.5);
+        hardware.leftBack.setPower(-0.5);
+        hardware.rightBack.setPower(0.5);
+    }
+
+    @Override
+    public void turnLeft(RobotHardware hardware) {
+        hardware.leftFront.setPower(0.5);
+        hardware.rightFront.setPower(-0.5);
+        hardware.leftBack.setPower(0.5);
+        hardware.rightBack.setPower(-0.5);
+    }
+
+    @Override
+    public void stop(RobotHardware hardware) {
+        hardware.leftFront.setPower(0);
+        hardware.rightFront.setPower(0);
+        hardware.leftBack.setPower(0);
+        hardware.rightBack.setPower(0);
+    }
 }

b256e6272b17cbeed4f7aa71a741c1cb5c7848b3
yashp123456 <136022696+yashp123456@users.noreply.github.com>
2025-07-21 21:18:47 -0400

    Create OneSidedClaw.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code101/OneSidedClaw.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "OneSidedClaw")
+public class OneSidedClaw extends LinearOpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+    public FtcDashboard ftcDashboard;
+
+    private Servo claw1;
+    @Override
+    public void runOpMode() {
+        this.ftcDashboard = FtcDashboard.getInstance();
+        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        // Initialize claw servo
+        claw1 = hardwareMap.get(Servo.class, "claw1");
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        claw1.setPosition(0.0);
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                claw1.setPosition(0.75);
+                telemetry.addData("claw open", "clicked");
+            } else if (gamepad1.b) {
+                claw1.setPosition(0.25);
+                telemetry.addData("claw close", "clicked");
+            }
+            telemetry.update();
+        }
+    }
+}
